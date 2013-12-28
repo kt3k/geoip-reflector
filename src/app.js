59b1330b385ec767b43d42197178cc6e384d0762
Yoshiya Hinosawa <stibium121@gmail.com>
2013-12-28 17:51:01 +0900

    Fix for x-forwarded-for header

--- src/app.js
+++ src/app.js
@@ -14,7 +14,7 @@ app.use(logfmt.requestLogger());
 
 app.get('/', function(req, res) {
 
-    var ip = req.headers['X-Forwarded-For'] || req.connection.remoteAddress;
+    var ip = req.header('x-forwarded-for') || req.connection.remoteAddress;
 
     var geo = geoip.lookup(ip);
 
