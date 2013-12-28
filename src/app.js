67e3939e86254a16f1ef3fc2d88b562fee22d7af
Yoshiya Hinosawa <stibium121@gmail.com>
2013-12-28 17:48:05 +0900

    Fallback to original ip address

--- src/app.js
+++ src/app.js
@@ -14,7 +14,7 @@ app.use(logfmt.requestLogger());
 
 app.get('/', function(req, res) {
 
-    var ip = req.headers['X-Forwarded-For'];
+    var ip = req.headers['X-Forwarded-For'] || req.connection.remoteAddress;
 
     var geo = geoip.lookup(ip);
 
