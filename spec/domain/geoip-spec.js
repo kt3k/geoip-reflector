176a72082be9b871193e6466ed8740177cd4cfec
Yoshiya Hinosawa <stibium121@gmail.com>
2013-12-29 18:02:34 +0900

    Add model tests

--- spec/domain/geoip-spec.js
+++ spec/domain/geoip-spec.js
@@ -2,12 +2,41 @@
 var chai = require('chai');
 var expect = chai.expect;
 
-var Geoip = require('../../src/domain/geoip.js')
+var Geoip = require('../../src/domain/geoip.js');
+
+var describe = global.describe;
+var it = global.it;
 
 describe('Geoip', function () {
+    'use strict';
 
     it('is exists', function () {
         expect(Geoip).not.to.equal(null);
+    });
+
+    describe('toObject', function () {
+
+        it('returns object representation of the instance', function () {
+
+            var geoip = new Geoip('0.0.0.0', 'AB');
+
+            expect(geoip.toObject()).to.deep.equal({
+                ipAddr: '0.0.0.0',
+                countryCode: 'AB'
+            });
+
+        });
+    });
+
+    describe('toJson', function () {
+
+        it('returns json representation of the instance', function () {
+
+            var geoip = new Geoip('0.0.0.0', 'AB');
+
+            expect(geoip.toJson()).to.equal('{"ipAddr":"0.0.0.0","countryCode":"AB"}');
+
+        });
     });
 
 });
