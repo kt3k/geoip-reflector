0ca235a61f0ebebf7ac66daf649d478209ad6b0d
Yoshiya Hinosawa <stibium121@gmail.com>
2013-12-29 20:57:25 +0900

    Use domain module

--- src/app.js
+++ src/app.js
@@ -2,24 +2,12 @@
 
 var express = require('express');
 var logfmt = require('logfmt');
-var geoip = require('geoip-lite');
+
+var GeoipFactory = require('./domain/geoip-factory');
 
 
 // create express app object
 var app = express();
-
-
-var getRemoteAddress = function (req) {
-    'use strict';
-
-    var forwarded = req.header('x-forwarded-for');
-
-    if (typeof forwarded === 'string') {
-        return forwarded.split(',')[0];
-    }
-
-    return req.connection.remoteAddress;
-};
 
 
 // set logger
@@ -28,18 +16,9 @@ app.use(logfmt.requestLogger());
 app.get('/', function (req, res) {
     'use strict';
 
-    var ip = getRemoteAddress(req);
+    var geoip = GeoipFactory.createByRequest(req);
 
-    var geo = geoip.lookup(ip);
-
-    var result = {
-        ipAddr: ip,
-        countryCode: geo != null ? geo.country : null
-    };
-
-    res.setHeader('Content-Type', 'application/json');
-
-    res.send(JSON.stringify(result, null, 4));
+    res.send(geoip.toJson());
 });
 
 var port = process.env.PORT || 5000;
